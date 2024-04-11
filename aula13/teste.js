501a991a4370065495ae3c5ceed64e6ebf335cb0
Matheus de Araújo Barreto <165087391+matheusaraujobarreto@users.noreply.github.com>
2024-04-11 13:53:12 -0300

    começando a usar laços

---
+++ aula13/teste.js
@@ -0,0 +1,16 @@
+/*console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')*/
+
+var c = 1
+while(c <= 6){
+    console.log(`Contagem numero ${c}`)
+    c++
+}/*Dá pra fazer asssim*/ 
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 6)
+/*Ou assim*/ 
