b16c65dfcc433a4757db4ee26486476e3ae14586
Matheus de Araújo Barreto <165087391+matheusaraujobarreto@users.noreply.github.com>
2024-04-11 15:36:16 -0300

    atividadeszinhas de repetições

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,1 +1,25 @@
-alert('ola')
+function contar(){
+    var ini = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var sal = document.getElementById('salto')
+    var resultado = document.getElementById('res')
+    resultado.innerHTML = 'Contando:'
+    var i = Number(ini.value)
+    var f = Number(fim.value)
+    var s = Number(sal.value)
+    if(sal<=0){
+        alert('Passo inválido. Vou considerar salto de 1')
+        s = 1
+    }
+    if(i<f){//crescendo
+        for(var c = i; c <= f; c += s){
+            resultado.innerHTML += `${c} \u{1F449} `
+        }
+    }
+    else{//decrescendo
+        for(var c = i; c >= f; c -= s){
+            resultado.innerHTML += `${c} \u{1F449} `
+        }
+    }
+    resultado.innerHTML += `\u{1F3C1}`
+}
--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -1,1 +1,13 @@
-alert('ola')
+function tabuada(){
+    var num = document.querySelector('input#numero')
+    var tab = document.getElementById('seltab')
+    var n1 = Number(num.value)
+    var c = 1
+    tab.innerHTML = ''
+    while(c <= 10){
+        let item = document.createElement('option')
+        item.text = `${n1} x ${c} = ${n1*c}`
+        tab.appendChild(item)
+        c++
+    }
+}
